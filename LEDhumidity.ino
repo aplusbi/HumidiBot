5920fdafad6a7194245bff9a39905b46c628a1fd
Niki Yoshiuchi <nyoshiuchi@gmail.com>
2020-12-27 13:33:42 -0800

    Adding RTC module
    Adding a clock to sleep functionality over night. Also adding controls
    for turning on and off lights.

--- LEDhumidity.ino
+++ LEDhumidity.ino
@@ -1,3 +1,5 @@
+#include <RTClib.h>
+
 // Example testing sketch for various DHT humidity/temperature sensors
 // Written by ladyada, public domain
 
@@ -18,7 +20,11 @@
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-#define HUMPIN 3
+#define HUMPIN 3    // Pin for the humidifier
+#define LITPIN 5    // Pin for the lights
+
+#define ONTIME 7    // Turn on at 7am
+#define OFFTIME 18  // Turn off at 6pm
 
 // Connect pin 1 (on the left) of the sensor to +5V
 // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
@@ -36,19 +42,62 @@ DHT dht(DHTPIN, DHTTYPE);
 // Connect via i2c, default address #0 (A0-A2 not jumpered)
 Adafruit_LiquidCrystal lcd(0);
 
+RTC_DS3231 rtc;
+
 void setup() {
   Serial.begin(9600);
   Serial.println(F("DHTxx test!"));
 
+  rtc.begin();
   dht.begin();
-  pinMode(3, OUTPUT);
+  
+  pinMode(HUMPIN, OUTPUT);
+  pinMode(LITPIN, OUTPUT);
+  
   lcd.begin(16, 2);
-  lcd.print("Humidity bot");
   lcd.setBacklight(LOW);
 }
 
+void printLCDHeader(DateTime now) {
+  lcd.setCursor(0, 0);
+  
+  lcd.print("HumidiBot ");
+  lcd.print(now.hour());
+  lcd.print(":");
+  lcd.print(now.minute());
+}
+
+void turnOffAndWait(DateTime now) {
+  lcd.print("Sleeping...");
+  
+  digitalWrite(HUMPIN, LOW);
+  digitalWrite(LITPIN, LOW);
+
+  // Get the time for when humidity bot should turn back on
+  DateTime turnOn;
+  if (now.hour() > OFFTIME) {
+    DateTime tomorrow = now + TimeSpan(1, 0, 0, 0);
+    turnOn = DateTime(tomorrow.year(), tomorrow.month(), tomorrow.day(), ONTIME);
+  } else {
+    turnOn = DateTime(now.year(), now.month(), now.day(), ONTIME, 1);
+  }
+
+  // Calculate how long the bot should sleep
+  TimeSpan sleepTime = turnOn - now;
+  delay(sleepTime.totalseconds() * 1000);
+}
 
 void loop() {
+  // Check the time, Humidity Bot is only operational between 7am and 6pm
+  DateTime now = rtc.now();
+  if (now.hour() > OFFTIME || now.hour() < ONTIME) {
+    turnOffAndWait(now);
+  } else {
+    digitalWrite(LITPIN, HIGH);
+  }
+
+  printLCDHeader(now);
+  
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
@@ -57,23 +106,18 @@ void loop() {
   // Read temperature as Fahrenheit (isFahrenheit = true)
   float f = dht.readTemperature(true);
 
-  if (h > 85) {
-    digitalWrite(HUMPIN, LOW);
-  }
-  else if (h < 75) {
-    digitalWrite(HUMPIN, HIGH);
-  }
-
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
+  if (h > 85) {
+    digitalWrite(HUMPIN, LOW);
+  }
+  else if (h < 75) {
+    digitalWrite(HUMPIN, HIGH);
+  }
 
   lcd.setCursor(0, 1);
   lcd.print(h);
@@ -88,13 +132,7 @@ void loop() {
   Serial.print(t);
   Serial.print(F("°C "));
   Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F"));
   
   // Wait a few seconds between measurements.
   delay(2000);
-  //delay(1000*60*5);
 }
