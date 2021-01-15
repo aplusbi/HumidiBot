1b4eb82d623fc2e4491a1ac45e68a61e7a852f70
Niki Yoshiuchi <nyoshiuchi@gmail.com>
2021-01-14 21:40:23 -0800

    Adding timer interrupts to turn on and off
    The robot now goes into sleep mode and wakes up via interrupts
    controlled by the RTC module.

--- LEDhumidity.ino
+++ LEDhumidity.ino
@@ -1,4 +1,5 @@
 #include <RTClib.h>
+#include <avr/sleep.h>
 
 // Example testing sketch for various DHT humidity/temperature sensors
 // Written by ladyada, public domain
@@ -11,6 +12,14 @@
 #include "Wire.h"
 #include "Adafruit_LiquidCrystal.h"
 
+typedef void (*IntFunction)(void);
+
+enum BotState {
+  ACTIVE,
+  TURN_ON,
+  TURN_OFF
+};
+
 #define DHTPIN 2     // Digital pin connected to the DHT sensor
 // Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
 // Pin 15 can work but DHT must be disconnected during program upload.
@@ -20,11 +29,9 @@
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-#define HUMPIN 3    // Pin for the humidifier
+#define HUMPIN 4    // Pin for the humidifier
 #define LITPIN 5    // Pin for the lights
-
-#define ONTIME 7    // Turn on at 7am
-#define OFFTIME 18  // Turn off at 6pm
+#define ARMPIN 3    // Pin for the alarm Interrupt
 
 // Connect pin 1 (on the left) of the sensor to +5V
 // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
@@ -46,16 +53,93 @@ RTC_DS3231 rtc;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println(F("DHTxx test!"));
 
   rtc.begin();
   dht.begin();
   
   pinMode(HUMPIN, OUTPUT);
   pinMode(LITPIN, OUTPUT);
+  pinMode(ARMPIN, INPUT_PULLUP); // Set alarm pin as pullup
   
   lcd.begin(16, 2);
   lcd.setBacklight(LOW);
+
+  initializeAlarms();
+}
+
+volatile BotState ALARM_FLAG = ACTIVE;
+DateTime TURN_OFF_TIME = DateTime(2020, 01, 01, 18);
+DateTime TURN_ON_TIME = DateTime(2020, 01, 01, 7);
+
+void initializeAlarms() {
+  rtc.disableAlarm(1);
+  rtc.disableAlarm(2);
+  rtc.clearAlarm(1);
+  rtc.clearAlarm(2);
+
+  rtc.writeSqwPinMode(DS3231_OFF);
+
+  // Set the bot to turn off at 6pm.
+  // If the bot should start off, set the turn off time to 5 seconds from now.
+  DateTime now = rtc.now();
+  if (now.hour() >= TURN_OFF_TIME.hour() || now.hour() < TURN_ON_TIME.hour()) {
+    ALARM_FLAG = TURN_OFF;
+  } else {
+    ALARM_FLAG = TURN_ON;
+  }
+}
+
+void initializeAlarm(DateTime alarm, IntFunction isr) {
+  rtc.setAlarm1(alarm, DS3231_A1_Hour);
+  noInterrupts();
+  attachInterrupt(digitalPinToInterrupt(ARMPIN), isr, LOW);
+  interrupts();
+}
+
+void turnOffIsr() {
+  ALARM_FLAG = TURN_OFF;
+  detachInterrupt(digitalPinToInterrupt(ARMPIN));
+}
+
+void turnOnIsr() {
+  ALARM_FLAG = TURN_ON;
+  sleep_disable();
+  detachInterrupt(digitalPinToInterrupt(ARMPIN));
+}
+
+void turnOff() {
+    ALARM_FLAG = ACTIVE;
+    
+    lcd.clear();
+    lcd.print("Sleep until 7:00");
+    
+    sleep_enable();
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
+    Serial.print("Turning off at ");
+    Serial.println(rtc.now().timestamp());
+  
+    digitalWrite(HUMPIN, LOW);
+    digitalWrite(LITPIN, LOW);
+
+    rtc.clearAlarm(1);
+    initializeAlarm(TURN_ON_TIME, turnOnIsr);
+    
+    sleep_cpu();
+}
+
+void turnOn() {
+    ALARM_FLAG = ACTIVE;
+
+    lcd.clear();
+    
+    Serial.print("Turning on at ");
+    Serial.println(rtc.now().timestamp());
+  
+    digitalWrite(LITPIN, HIGH);
+    
+    rtc.clearAlarm(1);
+    initializeAlarm(TURN_OFF_TIME, turnOffIsr);
 }
 
 void printLCDHeader(DateTime now) {
@@ -67,34 +151,17 @@ void printLCDHeader(DateTime now) {
   lcd.print(now.minute());
 }
 
-void turnOffAndWait(DateTime now) {
-  lcd.print("Sleeping...");
-  
-  digitalWrite(HUMPIN, LOW);
-  digitalWrite(LITPIN, LOW);
-
-  // Get the time for when humidity bot should turn back on
-  DateTime turnOn;
-  if (now.hour() > OFFTIME) {
-    DateTime tomorrow = now + TimeSpan(1, 0, 0, 0);
-    turnOn = DateTime(tomorrow.year(), tomorrow.month(), tomorrow.day(), ONTIME);
-  } else {
-    turnOn = DateTime(now.year(), now.month(), now.day(), ONTIME, 1);
-  }
-
-  // Calculate how long the bot should sleep
-  TimeSpan sleepTime = turnOn - now;
-  delay(sleepTime.totalseconds() * 1000);
-}
-
 void loop() {
   // Check the time, Humidity Bot is only operational between 7am and 6pm
   DateTime now = rtc.now();
-  if (now.hour() > OFFTIME || now.hour() < ONTIME) {
-    turnOffAndWait(now);
-  } else {
-    digitalWrite(LITPIN, HIGH);
+  
+  if (ALARM_FLAG == TURN_OFF) {
+    turnOff();
+  } else if (ALARM_FLAG == TURN_ON) {
+    turnOn();
   }
+  
+  Serial.println(now.timestamp());
 
   printLCDHeader(now);
   
@@ -109,6 +176,7 @@ void loop() {
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println(F("Failed to read from DHT sensor!"));
+    delay(1000);
     return;
   }
 
